76aa1dedf04e99309846fb64ae4025efa5a34169
Johannes <bi3ri@gmx.de>
2024-07-15 12:02:21 +0200

    Update extractFileInfo.ts

--- app/services/guide-builder/builders/extractFileInfo.ts
+++ app/services/guide-builder/builders/extractFileInfo.ts
@@ -2,12 +2,12 @@ export interface FileInfo {
     lng: string;
     fileName: string;
     fileExtension: string;
-    fileType?: string; // Optional fileType field
+    fileType?: string; 
 }
 
 export const extractFileInfo = (path: string): FileInfo => {
     const result: FileInfo = { lng: "", fileName: "", fileExtension: "" };
-    const parts = path.split('/'); // Split the path into segments
+    const parts = path.split('/'); 
 
     if (parts.length > 2 && parts[0] === "content") {
         result.lng = parts[1];
@@ -15,20 +15,17 @@ export const extractFileInfo = (path: string): FileInfo => {
         result.lng = "none";
     }
 
-    const filenameWithExtension = parts[parts.length - 1]; // The filename with extension is the last segment
+    const filenameWithExtension = parts[parts.length - 1]; 
 
-    // Split the last segment into filename, fileType (if exists), and extension
     const fileNameParts = filenameWithExtension.split('.');
     let fileType;
     if (fileNameParts.length === 3) {
-        // If there are 3 parts, then a fileType specifier exists
         [result.fileName, fileType, result.fileExtension] = fileNameParts;
-        result.fileType = fileType; // Assign the fileType if it exists
+        result.fileType = fileType; 
     } else if (fileNameParts.length === 2) {
-        // If only 2 parts, then no fileType specifier, just filename and extension
         [result.fileName, result.fileExtension] = fileNameParts;
     } else {
-        result.fileName = fileNameParts[0]; // If no extension, just assign the filename
+        result.fileName = fileNameParts[0];
         result.fileExtension = '';
     }
 
